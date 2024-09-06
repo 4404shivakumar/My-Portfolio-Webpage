9dc095f051b885cbf3a823c1e4771f9074cf62c7
4404shivakumar <4404shivakumar@gmail.com>
2024-09-06 16:34:01 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const sideMenu = document.querySelector('#sideMenu');
+
+function openMenu(){
+    sideMenu.style.transform = 'translateX(-0rem)';
+}
+function closeMenu(){
+    sideMenu.style.transform = 'translateX(16rem)';
+}
